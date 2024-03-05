25526997d5f24f7a8aeb3d358f535caefa34c31b
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-03-05 17:55:50 +0530

    added mailSender for sending otp to mail and pre middleware in otp schema

--- Server/models/OTP.js
+++ Server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,30 @@ const OTPSchema = new mongoose.Schema({
     default: Date.now(),
     expires: 5 * 60,
   },
+});
+
+// function to send email
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from Course Craft",
+      otp
+    );
+    console.log("Email sent Successfully: ", mailResponse);
+  } catch (error) {
+    // we include these string lines in the console of catch block to make the tester get exact information where the error is happening
+    // do include these while building a production build application/website
+    console.log("Error occured while sending the mails: ", error);
+    throw error;
+  }
+}
+
+// using pre middleware which lets us send verification mail with the given data before saving the document to database
+// this.email and this.otp -> current object data
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
 });
 
 module.exports = mongoose.model("OTP", OTPSchema);
---
+++ Server/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+// from nodemailer documentation
+// this is the setup for the pre middleware used in the OTP model
+// we have made this mailSender function so that we can send otp in mail to the user
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: "Course Craft || Project - by Saurabh",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+    console.log(info);
+    return info;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = mailSender;
