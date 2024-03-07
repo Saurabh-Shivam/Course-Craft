2010d88aa6bd783286af1cfe88e8ab66f73a6f72
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-03-07 20:11:32 +0530

    created  Tag handler and CoursesHanlder along with imageUploader with many TODO items to do

---
+++ Server/controllers/Course.js
@@ -0,0 +1,139 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Tags");
+const User = require("../models//User");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+require("dotenv").config();
+
+// createCourse handler function
+exports.createCourse = async (req, res) => {
+  try {
+    // fetch data
+    const { courseName, courseDescription, whatYouWillLearn, price, tag } =
+      req.body;
+
+    // get thumbnail
+    const thumbnail = req.files.thumbnailImage;
+
+    // validation
+    if (
+      !courseName ||
+      !courseDescription ||
+      !whatYouWillLearn ||
+      !price ||
+      !tag ||
+      !thumbnail
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    //  check for instructor
+    // TODO: Verify that userID and instructor._id are same or different?
+    const userId = req.user.id;
+    const instructorDetails = await User.findById(userId);
+    console.log("Instructor Details: ", instructorDetails);
+
+    if (!instructorDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Instructor details not found",
+      });
+    }
+
+    // check given tag is valid or not
+    const tagDetails = await Tag.findById(tag);
+    if (!tagDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Tag details not found",
+      });
+    }
+
+    // Upload Image to cloudinary
+    const thumbnailImage = await uploadImageToCloudinary(
+      thumbnail,
+      process.env.FOLDER_NAME
+    );
+
+    // create an entry in db for new course
+    const newCourse = await Course.create({
+      courseName,
+      courseDescription,
+      instructor: instructorDetails._id,
+      whatYouWillLearn: whatYouWillLearn,
+      price,
+      tag: tagDetails._id,
+      thumbnail: thumbnailImage.secure_url,
+    });
+
+    // add new courses to the User schema of Instructor
+    await User.findByIdAndUpdate(
+      { _id: instructorDetails._id },
+      {
+        $push: {
+          courses: newCourse._id,
+        },
+      },
+      { new: true }
+    );
+
+    // TODO: update tag schema
+    await Tag.findByIdAndUpdate(
+      { _id: tagDetails._id },
+      {
+        $push: {
+          courses: newCourse._id,
+        },
+      },
+      { new: true }
+    );
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Course Created Successfully",
+      data: newCourse,
+    });
+  } catch (error) {
+    console.log("Error while creating course: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create course",
+      error: error.message,
+    });
+  }
+};
+
+// getAllCourse handler function
+exports.showAllCourses = async (req, res) => {
+  try {
+    // const allCourses = await Course.find(
+    //   {},
+    //   {
+    //     courseName: true,
+    //     price: true,
+    //     instructor: true,
+    //     ratingAndReviews: true,
+    //     studentsEnrolled: true,
+    //   }
+    // )
+    //   .populate("instructor")
+    //   .exec();
+    // TODO: Change the below statement incrementally, the above written code is better
+    const allCourses = await Course.find();
+    return res.status(200).json({
+      success: true,
+      message: "Courses retrieved successfully",
+      data: allCourses,
+    });
+  } catch (error) {
+    console.log("Error while fetching courses: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch courses",
+      error: error.message,
+    });
+  }
+};
