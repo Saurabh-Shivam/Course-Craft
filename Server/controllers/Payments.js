7b3e0d7b24519227bd5a50efc0e4ed4b043367d1
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-03-26 11:40:46 +0530

    completed payment controller

--- Server/controllers/Payments.js
+++ Server/controllers/Payments.js
@@ -95,4 +95,71 @@ exports.verifySignature = async (req, res) => {
   const webhookSecret = "12345678";
   // signature send by razorpay
   const signature = req.headers["x-razorpay-signature"];
+
+  // converting webhookSecret into digest
+  const shasum = crypto.createHmac("sha256", webhookSecret);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+
+  // checking the digest with the signature received from razorpay
+  if (signature === digest) {
+    console.log("Payment is Authorised");
+
+    const { courseId, userId } = req.body.payload.payment.entity.notes;
+    try {
+      // fulfill the action
+      // find the course and enroll the student in it
+      const enrolledCourse = await Course.findOneAndUpdate(
+        { _id: courseId },
+        { $push: { studentsEnrolled: userId } },
+        { new: true }
+      );
+
+      // validation
+      if (!enrolledCourse) {
+        return res.status(500).json({
+          success: false,
+          message: "Course not found",
+        });
+      }
+
+      console.log(enrolledCourse);
+
+      // find the student and add the course to their lsit of enrolled courses
+      const enrolledStudent = await User.findOneAndUpdate(
+        { _id: userId },
+        { $push: { courses: courseId } },
+        { new: true }
+      );
+      console.log(enrolledCourse);
+
+      // send the confirmation mail
+      // TODO: Need to attach this to our mail template created
+      const emailResponse = await mailSender(
+        enrolledStudent.email,
+        "Congratulations from CourseCraft",
+        "Congratulations, you are onboarded into new CourseCraft Course"
+      );
+      console.log(emailResponse);
+
+      // return response
+      return res.status(200).json({
+        success: true,
+        message: "Signature Verified and Course Added",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+  // if the signature doesn't match
+  else {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid request",
+    });
+  }
 };
